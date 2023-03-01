6371abd7f9121538a925c8444839aa5961b00c82
ndujesco <ugopeter26@gmail.com>
2023-03-01 15:06:39 +0100

    update

--- utils/custom-crypt.js
+++ utils/custom-crypt.js
@@ -1,36 +1,36 @@
-const axios = require("axios").default;
+// const axios = require("axios").default;
 
-customConfig = {
-  headers: {
-    "Content-Type": "application/json",
-  },
-};
+// customConfig = {
+//   headers: {
+//     "Content-Type": "application/json",
+//   },
+// };
 
-exports.hashPassword = async (password) => {
-  try {
-    const response = await axios.post(
-      "https://hashed-password.herokuapp.com/hash-password",
-      JSON.stringify({ password }),
-      customConfig
-    );
-    return response.data.hashedPassword;
-  } catch (error) {
-    console.log(error);
-  }
-};
+// exports.hashPassword = async (password) => {
+//   try {
+//     const response = await axios.post(
+//       "https://hashed-password.herokuapp.com/hash-password",
+//       JSON.stringify({ password }),
+//       customConfig
+//     );
+//     return response.data.hashedPassword;
+//   } catch (error) {
+//     console.log(error);
+//   }
+// };
 
-exports.comparePassword = async (hashedPassword, password) => {
-  try {
-    const response = await axios.post(
-      "https://hashed-password.herokuapp.com/check-password",
-      JSON.stringify({
-        hashedPassword,
-        password,
-      }),
-      customConfig
-    );
-    return response.data.matches;
-  } catch (error) {
-    console.log(error);
-  }
-};
+// exports.comparePassword = async (hashedPassword, password) => {
+//   try {
+//     const response = await axios.post(
+//       "https://hashed-password.herokuapp.com/check-password",
+//       JSON.stringify({
+//         hashedPassword,
+//         password,
+//       }),
+//       customConfig
+//     );
+//     return response.data.matches;
+//   } catch (error) {
+//     console.log(error);
+//   }
+// };
